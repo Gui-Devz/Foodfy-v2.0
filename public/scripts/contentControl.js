62ec1302def850d1111cf049b5918ddb716cb813
Guilherme <guilherme_devz@hotmail.com>
2020-08-24 22:18:14 -0300

    FEATURE:  creating the script that controls the css of the menu links activated

--- public/scripts/contentControl.js
+++ public/scripts/contentControl.js
@@ -1,19 +1,41 @@
 const divSteps = document.querySelectorAll(".step");
 
-for (const divStep of divSteps) {
+let menuOn = window.location.href;
+
+function activateMenuLinksCSS(arrayObj, filter, content) {
+  if (arrayObj.includes(`${filter}`)) {
+    const menuLinks = document.querySelectorAll(".menu div ul li");
+
+    console.log(menuLinks);
+
+    for (const link of menuLinks) {
+      if (link.firstChild.innerHTML == `${content}`) {
+        link.classList.toggle("active");
+      }
+    }
+  }
+}
+
+activateMenuLinksCSS(menuOn, "recipes", "Receitas");
+activateMenuLinksCSS(menuOn, "about", "Sobre");
+activateMenuLinksCSS(menuOn, "chefs", "Chefs");
+
+if (divSteps) {
+  for (const divStep of divSteps) {
     const button = divStep.querySelector(".info-content p");
 
     button.addEventListener("click", () => {
-        const content = divStep.querySelector("div");
+      const content = divStep.querySelector("div");
 
-        if (content.classList.contains("hidden")) {
-            content.classList.remove("hidden");
+      if (content.classList.contains("hidden")) {
+        content.classList.remove("hidden");
 
-            button.innerHTML = "Esconder";
-        } else {
-            content.classList.add("hidden");
+        button.innerHTML = "Esconder";
+      } else {
+        content.classList.add("hidden");
 
-            button.innerHTML = "Mostrar";
-        }
+        button.innerHTML = "Mostrar";
+      }
     });
+  }
 }
