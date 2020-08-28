a353971440a484988e46740683cce780c6a7b1e0
Guilherme <guilherme_devz@hotmail.com>
2020-08-28 20:02:10 -0300

    FIX: Show option to filter recipes only in the index and recipes pages

--- public/scripts/contentControl.js
+++ public/scripts/contentControl.js
@@ -1,12 +1,10 @@
 const divSteps = document.querySelectorAll(".step");
 
-let menuOn = window.location.href;
+let urlCheck = window.location.href;
 
 function activateMenuLinksCSS(arrayObj, filter, content) {
   if (arrayObj.includes(`${filter}`)) {
     const menuLinks = document.querySelectorAll(".menu div ul li");
-
-    console.log(menuLinks);
 
     for (const link of menuLinks) {
       if (link.firstChild.innerHTML == `${content}`) {
@@ -16,10 +14,8 @@ function activateMenuLinksCSS(arrayObj, filter, content) {
   }
 }
 
-activateMenuLinksCSS(menuOn, "recipes", "Receitas");
-activateMenuLinksCSS(menuOn, "about", "Sobre");
-activateMenuLinksCSS(menuOn, "chefs", "Chefs");
-
+/* RECIPE DETAILS PAGE
+-- This code allows to hide or show details about the recipe like the ingredients and the steps */
 if (divSteps) {
   for (const divStep of divSteps) {
     const button = divStep.querySelector(".info-content p");
@@ -39,3 +35,14 @@ if (divSteps) {
     });
   }
 }
+
+/* CODE THAT DEFINES WHAT PAGES SHOULD SHOW THE OPTION FOR THE FILTER OF RECIPES */
+if (urlCheck.includes("index") || urlCheck.includes("recipes")) {
+  const filterDiv = document.querySelector(".filter");
+
+  filterDiv.classList.toggle("show");
+}
+
+activateMenuLinksCSS(urlCheck, "recipes", "Receitas");
+activateMenuLinksCSS(urlCheck, "about", "Sobre");
+activateMenuLinksCSS(urlCheck, "chefs", "Chefs");
