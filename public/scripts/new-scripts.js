2b123b04f414fbbf2a8b3d3c2dd4bcc1ec9a02aa
Guilherme <guilherme_devz@hotmail.com>
2021-01-09 21:33:46 -0300

    Adjusting the object for verification of buttons of the forms

--- public/scripts/new-scripts.js
+++ public/scripts/new-scripts.js
@@ -14,11 +14,13 @@ const addingLinksToCards = {
 const addingFilterSearchH1 = {
   h1() {
     let filter = document.querySelector("#filter").dataset.filter;
-    const element = document.createElement("h1");
-    const wrapperGrid = document.querySelector(".wrapper-grid");
+    if (filter != "") {
+      const element = document.createElement("h1");
+      const wrapperGrid = document.querySelector(".wrapper-grid");
 
-    element.innerHTML = `Buscando por "${filter}"`;
-    wrapperGrid.insertBefore(element, wrapperGrid.firstChild);
+      element.innerHTML = `Buscando por "${filter}"`;
+      wrapperGrid.insertBefore(element, wrapperGrid.firstChild);
+    }
   },
 };
 
@@ -62,6 +64,39 @@ const controllingFieldsInput = {
 
     newField.children[0].value = "";
     steps.appendChild(newField);
+  },
+};
+
+const confirmationOfButtons = {
+  verifyingBeforeDeleteChef(e) {
+    const divDelete = document.querySelector(".delete");
+
+    if (divDelete.dataset.has_recipes == "false") {
+      const confirmation = confirm("Deseja excluir esse chef?!");
+
+      if (!confirmation) return e.preventDefault();
+    } else if (divDelete.dataset.has_recipes == "true") {
+      alert("Não é permitido excluir chefs que tenham receitas cadastradas!");
+
+      return e.preventDefault();
+    }
+  },
+
+  verifyingBeforeSavingChef(e) {
+    const confirmation = confirm("Deseja salvar esse chef?!");
+
+    if (!confirmation) return e.preventDefault();
+  },
+  verifyingBeforeDeleteRecipe(e) {
+    const confirmation = confirm("Deseja excluir essa receita?!");
+
+    if (!confirmation) return e.preventDefault();
+  },
+
+  verifyingBeforeSavingRecipe(e) {
+    const confirmation = confirm("Deseja salvar essa receita?!");
+
+    if (!confirmation) return e.preventDefault();
   },
 };
 
