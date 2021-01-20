d06fb76685dc8ba14246b609a13860a67d095cd2
Guilherme <guilherme_devz@hotmail.com>
2021-01-19 22:23:26 -0300

    ADD: creating the models functions for showing the recipe's files and the chef's avatar

--- src/app/models/file.js
+++ src/app/models/file.js
@@ -1,15 +1,23 @@
 const db = require("../../config/db");
 
 module.exports = {
-  savingFile(filename, file) {
+  showRecipeFiles(recipe_ID) {
     const query = `
-            INSERT INTO files (
-                name,
-                path
-            ) VALUES ($1, $2)
-            RETURNING id
-        `;
+        SELECT files.name, files.path
+        FROM recipe_files LEFT JOIN files ON (recipe_files.file_id = files.id)
+        WHERE recipe_files.recipe_id = $1
+      `;
 
-    return db.query(query, [filename, file]);
+    return db.query(query, [recipe_ID]);
+  },
+
+  showChefAvatar(chef_ID) {
+    const query = `
+        SELECT files.name, files.path
+        FROM chefs LEFT JOIN files ON (chefs.file_id = files.id)
+        WHERE chefs.id = $1
+      `;
+
+    return db.query(query, [chef_ID]);
   },
 };
