34c847ec4fbe3d493b2b1ec47f276dc27b1eda8f
Guilherme <guilherme_devz@hotmail.com>
2021-01-26 21:45:20 -0300

    FIX: refactoring the functions 'showChefsRecipes', 'showChef', 'showChefs'  now they return  promises instead of working with callbacks and the queries were updated according the changes in the database

--- src/app/models/chef.js
+++ src/app/models/chef.js
@@ -1,46 +1,41 @@
 const db = require("../../config/db");
 
 module.exports = {
-  showChefs(callback) {
+  showChefs() {
     const query = `
-        SELECT chefs.*, (SELECT count(*) FROM recipes WHERE chefs.id=recipes.chef_id) AS qt_recipes
+        SELECT chefs.*, (SELECT count(*) FROM recipes WHERE chefs.id=recipes.chef_id) AS qt_recipes,
+          files.path AS file_path, files.name AS file_name
         FROM chefs LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
-        GROUP BY chefs.id
+        INNER JOIN files ON (chefs.file_id = files.id)
+        GROUP BY chefs.id, files.path, files.name
     `;
 
-    db.query(query, function (err, results) {
-      if (err) throw `Database error! ${err}`;
-
-      callback(results.rows);
-    });
+    return db.query(query);
   },
 
-  showChef(id, callback) {
+  showChef(id) {
     const query = `
-        SELECT chefs.*, (SELECT count(*) FROM recipes WHERE chefs.id=recipes.chef_id) AS qt_recipes
+        SELECT DISTINCT ON (chefs.id)chefs.*, (SELECT count(*) FROM recipes WHERE chefs.id=recipes.chef_id) AS qt_recipes,
+          files.path AS file_path, files.name AS file_name
         FROM chefs LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
+        INNER JOIN files ON (chefs.file_id = files.id)
         WHERE chefs.id = $1
-        GROUP BY chefs.id
+        GROUP BY chefs.id, files.path, files.name
       `;
 
-    db.query(query, [id], function (err, result) {
-      if (err) throw `Database error! ${err}`;
-
-      callback(result.rows[0]);
-    });
+    return db.query(query, [id]);
   },
 
-  showChefsRecipes(id, callback) {
+  showChefsRecipes(id) {
     const query = `
-        SELECT recipes.*, chefs.name AS chef
-        FROM recipes LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-        WHERE chefs.id = $1
+        SELECT DISTINCT ON (recipes.id)recipes.*, chefs.name AS chef,
+          files.path AS file_path, files.name AS file_name
+        FROM recipe_files LEFT JOIN recipes ON (recipe_files.recipe_id = recipes.id)
+        INNER JOIN files ON (recipe_files.file_id = files.id)
+        INNER JOIN chefs ON (recipes.chef_id = chefs.id)
+        WHERE recipes.chef_id = $1
       `;
 
-    db.query(query, [id], function (err, results) {
-      if (err) throw `Database error! ${err}`;
-
-      callback(results.rows);
-    });
+    return db.query(query, [id]);
   },
 };
