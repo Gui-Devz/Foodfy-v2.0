e39d8ac4a332edf60cf6eb6e5b584a4ab57b6ad9
Guilherme <guilherme_devz@hotmail.com>
2021-02-09 10:39:30 -0300

    UPDATE: completing database and trigger funtions build

---
+++ foodfy_db.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS foodfy_db;
+CREATE DATABASE foodfy_db;
+
+CREATE TABLE "files" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "path" text NOT NULL
+);
+
+CREATE TABLE "recipe_files" (
+  "id" SERIAL PRIMARY KEY,
+  "recipe_id" int,
+  "file_id" int
+);
+
+CREATE TABLE "recipes" (
+  "id" SERIAL PRIMARY KEY,
+  "chef_id" int NOT NULL,
+  "title" text NOT NULL,
+  "ingredients" text[] NOT NULL,
+  "preparation" text[] NOT NULL,
+  "information" text,
+  "created_at" timestamp DEFAULT 'now()'
+  "updated_at" timestamp DEFAULT 'now()'
+);
+
+CREATE TABLE "chefs" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "file_id" int NOT NULL,
+  "created_at" timestamp DEFAULT 'now()'
+);
+
+-- Foreign keys
+ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+
+ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id");
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+
+-- CREATE PROCEDURE
+CREATE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+	NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- AUTO updated_at on recipes
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON recipes
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
