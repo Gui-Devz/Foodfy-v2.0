b05d04af603b80aeb7152a6952d7dcc2e72d99ba
Guilherme <guilherme_devz@hotmail.com>
2021-02-24 17:16:25 -0300

    UPDATE: fixing some silly mistakes

--- foodfy_db.sql
+++ foodfy_db.sql
@@ -21,37 +21,37 @@ CREATE TABLE "recipes" (
   "ingredients" text[] NOT NULL,
   "preparation" text[] NOT NULL,
   "information" text,
-  "created_at" timestamp DEFAULT 'now()'
-  "updated_at" timestamp DEFAULT 'now()'
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "chefs" (
   "id" SERIAL PRIMARY KEY,
   "name" text NOT NULL,
   "file_id" int NOT NULL,
-  "created_at" timestamp DEFAULT 'now()'
+  "created_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
-  "name" int NOT NULL,
+  "name" text NOT NULL,
   "email" text NOT NULL,
-  "passord" text NOT NULL,
+  "password" text NOT NULL,
   "reset_token" text,
   "reset_token_expires" text,
   "is_admin" boolean DEFAULT 'false',
-  "created_at" timestamp DEFAULT 'now()'
-  "updated_at" timestamp DEFAULT 'now()'
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
 -- Foreign keys
 ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
 
-ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id"); ON DELETE CASCADE;
-
-ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id") ON DELETE CASCADE;
+ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
 
