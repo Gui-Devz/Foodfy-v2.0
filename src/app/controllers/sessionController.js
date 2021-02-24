acab0b0987b865ba10976b26060bc94197e9b3ca
Guilherme <guilherme_devz@hotmail.com>
2021-02-24 20:05:05 -0300

    ADD: creating the function 'login'

--- src/app/controllers/sessionController.js
+++ src/app/controllers/sessionController.js
@@ -2,4 +2,12 @@ module.exports = {
   loginForm(req, res) {
     return res.render("session/login");
   },
+
+  async login(req, res) {
+    const userID = req.user.id;
+    req.session.userID = userID;
+
+    console.log("yeah!");
+    return res.redirect("/users");
+  },
 };
