08882ded451ef0dbbbee3c50c8780aff5484d10d
Guilherme <guilherme_devz@hotmail.com>
2021-02-24 17:24:34 -0300

    ADD: creating the route '/login'

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,8 @@
-// const express = require("express");
-// const admin = require("./app/controllers/admin");
-// const recipes = require("./app/controllers/recipes");
-// const chefs = require("./app/controllers/chefs");
+const express = require("express");
+const Session = require("../app/controllers/sessionController");
 
-// const routes = express.Router();
+const routes = express.Router();
 
-// ADMIN-SESSION
+routes.get("/login", Session.loginForm);
 
-// RECIPES PAGES
-// routes.get("/admin/recipes", admin.index);
-
-// routes.get("/admin/recipes/create", recipes.create);
-
-// routes.get("/admin/recipes/:id", recipes.showAdmin);
-
-// routes.get("/admin/recipes/:id/edit", recipes.edit);
+module.exports = routes;
