9b4485d6302db0422d04827af58b407b47491242
Guilherme <guilherme_devz@hotmail.com>
2021-03-02 18:52:06 -0300

    ADD: adding the middlewares for checking all that is needed before trying to login

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,18 +1,23 @@
 const express = require("express");
 
 const sessionController = require("../app/controllers/sessionController");
-const { login } = require("../app/middlewares/validators/session");
+const {
+  checkFormForgot,
+  checkFormLogin,
+  checkFormReset,
+} = require("../app/middlewares/validators/session");
+const { isLogged } = require("../app/middlewares/validators/users");
 
 const routes = express.Router();
 
 //SESSION ROUTES
-routes.get("/login", sessionController.loginForm);
-routes.get("/forgot-password", sessionController.forgotForm);
-routes.get("/reset-password", sessionController.resetForm);
-routes.get("/logout", sessionController.logout);
+routes.get("/login", isLogged, sessionController.loginForm);
+routes.get("/forgot-password", isLogged, sessionController.forgotForm);
+routes.get("/reset-password", isLogged, sessionController.resetForm);
+routes.get("/logout", isLogged, sessionController.logout);
 
-routes.post("/login", login, sessionController.login);
-routes.post("/forgot-password", login, sessionController.forgot);
-routes.post("/reset-password", login, sessionController.reset);
+routes.post("/login", checkFormLogin, sessionController.login);
+routes.post("/forgot-password", checkFormForgot, sessionController.forgot);
+routes.post("/reset-password", checkFormReset, sessionController.reset);
 
 module.exports = routes;
