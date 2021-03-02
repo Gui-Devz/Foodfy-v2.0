850648806b142941a33ec2a9b28112d2e4ec65a8
Guilherme <guilherme_devz@hotmail.com>
2021-03-02 18:43:19 -0300

    UPDATE: handling error in case user not found in the 'index' function

--- src/app/controllers/profileController.js
+++ src/app/controllers/profileController.js
@@ -1,9 +1,15 @@
+const { renderingRecipesWithOnlyOneFile } = require("../../lib/utils");
 const User = require("../models/user");
 
 module.exports = {
   async index(req, res) {
-    const user = await User.findUser({ where: { id: req.session.userID } });
+    const user = await User.find({ where: { id: req.session.userID } });
 
-    return res.render("admin/users/profile", { userLogged: user, user });
+    return res.render("admin/users/profile", {
+      userIsAdmin: req.user.is_admin,
+      user: req.user,
+    });
   },
+
+  async put(req, res) {},
 };
