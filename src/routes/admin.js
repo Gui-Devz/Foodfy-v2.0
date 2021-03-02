b9a63399382e50c88546a9af921cb46736f2bc0c
Guilherme <guilherme_devz@hotmail.com>
2021-03-01 23:16:55 -0300

    UPDATE: re-thinking where the routes of session will be placed. Decided to create a user.js file to allocate the session's routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,18 +6,11 @@ const chefsController = require("../app/controllers/chefsController");
 const usersController = require("../app/controllers/usersController");
 const profileController = require("../app/controllers/profileController");
 
-const sessionController = require("../app/controllers/sessionController");
-const { login } = require("../app/middlewares/validators/session");
 const { isLogged, isAdmin } = require("../app/middlewares/validators/users");
 
 const routes = express.Router();
 
 routes.get("/", isLogged, adminController.index);
-
-//SESSION ROUTES
-routes.get("/login", sessionController.loginForm);
-routes.post("/login", login, sessionController.login);
-routes.get("/logout", sessionController.logout);
 
 //PROFILE ROUTES
 routes.get("/profile", isLogged, profileController.index);
@@ -28,18 +21,15 @@ routes.get("/users", isAdmin, usersController.list);
 routes.get("/users/create", isAdmin, usersController.create);
 routes.get("/users/:id/edit", isAdmin, usersController.edit);
 
+routes.post("/users", usersController.post);
+routes.put("/users", usersController.put);
+routes.delete("/users", usersController.delete);
+
 //RECIPES ROUTES
 routes.get("/recipes/create", isLogged, recipesController.create);
 routes.get("/recipes/:id", isLogged, adminController.showRecipe);
 routes.get("/recipes/:id/edit", isLogged, recipesController.edit);
 
-//CHEFS ROUTES
-routes.get("/chefs", isLogged, adminController.listChef);
-routes.get("/chefs/create", isAdmin, chefsController.create);
-routes.get("/chefs/:id", isLogged, chefsController.show);
-routes.get("/chefs/:id/edit", isAdmin, chefsController.edit);
-
-//ADMIN RECIPES FORM
 routes.post(
   "/recipes",
   isLogged,
@@ -54,7 +44,12 @@ routes.put(
 );
 routes.delete("/recipes", isLogged, recipesController.delete);
 
-//ADMIN CHEFS FORM
+//CHEFS ROUTES
+routes.get("/chefs", isLogged, adminController.listChef);
+routes.get("/chefs/create", isAdmin, chefsController.create);
+routes.get("/chefs/:id", isLogged, chefsController.show);
+routes.get("/chefs/:id/edit", isAdmin, chefsController.edit);
+
 routes.post(
   "/chefs",
   isLogged,
@@ -63,10 +58,5 @@ routes.post(
 );
 routes.put("/chefs", isLogged, multer.array("avatar", 1), chefsController.put);
 routes.delete("/chefs", isLogged, chefsController.delete);
-
-//ADMIN USERS FORM
-routes.post("/users", usersController.post);
-routes.put("/users", usersController.put);
-routes.delete("/users", usersController.delete);
 
 module.exports = routes;
