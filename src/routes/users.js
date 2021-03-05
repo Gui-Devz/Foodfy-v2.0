c4fb35af3e6a3a74a9982b4a3d26c670ed8f5a89
Guilherme <guilherme_devz@hotmail.com>
2021-03-05 20:23:24 -0300

    UPDATE: adding the validator function 'login' in all the routes GET

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,15 +6,15 @@ const {
   checkFormLogin,
   checkFormReset,
 } = require("../app/middlewares/validators/session");
-const { isLogged } = require("../app/middlewares/validators/users");
+const { login } = require("../app/middlewares/validators/users");
 
 const routes = express.Router();
 
 //SESSION ROUTES
-routes.get("/login", isLogged, sessionController.loginForm);
-routes.get("/forgot-password", isLogged, sessionController.forgotForm);
-routes.get("/reset-password", isLogged, sessionController.resetForm);
-routes.get("/logout", isLogged, sessionController.logout);
+routes.get("/login", login, sessionController.loginForm);
+routes.get("/forgot-password", login, sessionController.forgotForm);
+routes.get("/reset-password", login, sessionController.resetForm);
+routes.get("/logout", login, sessionController.logout);
 
 routes.post("/login", checkFormLogin, sessionController.login);
 routes.post("/forgot-password", checkFormForgot, sessionController.forgot);
