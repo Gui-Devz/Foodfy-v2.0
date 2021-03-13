6fc9829ad220e57657d85eeae734c21203d8be5f
Guilherme <guilherme_devz@hotmail.com>
2021-03-13 20:24:53 -0300

    UPDATE: refactoring the name of the function 'validationOfChefName', now it's 'removingWhiteSpacesInBeginningAndEnding'

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,34 +1,34 @@
 module.exports = {
-  age(timestamp) {
-    const today = new Date();
-    const birthDate = new Date(timestamp);
+  // age(timestamp) {
+  //   const today = new Date();
+  //   const birthDate = new Date(timestamp);
 
-    //2020 - 1997 = 23
-    let age = today.getUTCFullYear() - birthDate.getFullYear();
-    const month = today.getUTCMonth() - birthDate.getMonth();
+  //   //2020 - 1997 = 23
+  //   let age = today.getUTCFullYear() - birthDate.getFullYear();
+  //   const month = today.getUTCMonth() - birthDate.getMonth();
 
-    if (month < 0 || (month == 0 && today.getUTCDate() < birthDate.getDate())) {
-      age = age - 1;
-    }
+  //   if (month < 0 || (month == 0 && today.getUTCDate() < birthDate.getDate())) {
+  //     age = age - 1;
+  //   }
 
-    return age;
-  },
+  //   return age;
+  // },
 
-  formatBrowser(timestamp) {
-    const date = new Date(timestamp);
-    const day = `0${date.getUTCDate()}`.slice(-2);
-    const month = `0${date.getUTCMonth() + 1}`.slice(-2);
-    const year = `${date.getUTCFullYear()}`;
+  // formatBrowser(timestamp) {
+  //   const date = new Date(timestamp);
+  //   const day = `0${date.getUTCDate()}`.slice(-2);
+  //   const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+  //   const year = `${date.getUTCFullYear()}`;
 
-    return {
-      day: day,
-      month: month,
-      year: year,
-      iso: `${year}-${month}-${day}`,
-      birthday: `${day}/${month}`,
-      format: `${day}/${month}/${year}`,
-    };
-  },
+  //   return {
+  //     day: day,
+  //     month: month,
+  //     year: year,
+  //     iso: `${year}-${month}-${day}`,
+  //     birthday: `${day}/${month}`,
+  //     format: `${day}/${month}/${year}`,
+  //   };
+  // },
 
   //Function guarantees that the arrays are set for the database
   arrayDB(array) {
@@ -58,10 +58,10 @@ module.exports = {
     return newInputs;
   },
 
-  validationOfChefName(name) {
-    let newName = name.replace(/^\s+|\s+$/g, "");
+  removingWhiteSpacesInBeginningAndEnding(value) {
+    let newValue = value.replace(/^\s+|\s+$/g, "");
 
-    return newName;
+    return newValue;
   },
 
   validationOfBlankFields(fields) {
