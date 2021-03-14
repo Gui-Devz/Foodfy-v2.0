40e9a474a8ad01fe3c2d9357fac52d31992f6688
Guilherme <guilherme_devz@hotmail.com>
2021-03-14 19:20:37 -0300

    UPDATE: adding the final version of the function 'post'

--- src/app/controllers/usersController.js
+++ src/app/controllers/usersController.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 const User = require("../models/user");
+const mailer = require("../../config/mailer");
 
 const { removingWhiteSpacesInBeginningAndEnding } = require("../../lib/utils");
 
@@ -40,22 +41,52 @@ module.exports = {
       const { name, email, is_admin } = req.body;
 
       const password = crypto.randomBytes(5).toString("hex");
+      const token = crypto.randomBytes(20).toString("hex");
+
+      let now = new Date();
+      now = now.setHours(now.getHours() + 1);
 
       const user = {
         name: removingWhiteSpacesInBeginningAndEnding(name),
         email: removingWhiteSpacesInBeginningAndEnding(email),
         password: password,
         is_admin: is_admin ? true : false,
+        reset_token: token,
+        reset_token_expires: now,
       };
 
       await User.saving(user);
 
       const users = await User.find();
 
+      const mailMessage = {
+        from: "no-reply@foodfy.com.br",
+        to: `${user.email}`,
+        subject: "Conta cadastrada com sucesso!",
+        html: `
+          <h1 style="text-align=center">
+            Sua conta foi registrada com sucesso!
+          </h1>
+          <p>
+            Seu email é ${user.email} e sua senha é ${password}.
+            <br>
+            Recomendamos que troque a sua senha o quanto antes, faça isso
+            <a href="http://localhost:3000/users/reset-password?token=${token}">aqui</a>.
+            <br>
+            <h2>
+              Esse link tem validade de 1 hora.
+            </h2>
+          </p>
+          `,
+      };
+
+      await mailer.sendMail(mailMessage);
+
       return res.render("admin/users/list", {
         users: users,
         userIsAdmin: req.user.is_admin,
-        success: "Usuário Cadastrado com sucesso!",
+        success:
+          "Usuário Cadastrado com sucesso! Por favor verifique o seu email.",
       });
 
       //console.log(user);
