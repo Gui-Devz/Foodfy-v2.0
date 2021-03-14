e23e52d73bacb2f4e895661f3772d952de74f3fb
Guilherme <guilherme_devz@hotmail.com>
2021-03-14 19:21:39 -0300

    UPDATE: adding the final version of the function 'checkFormReset'

--- src/app/middlewares/validators/session.js
+++ src/app/middlewares/validators/session.js
@@ -17,25 +17,16 @@ async function checkFormLogin(req, res, next) {
 
     const user = await User.find({ where: { email } });
 
-    if (!user) {
+    if (!user[0]) {
       return res.render("session/login", {
         error: "Email não encontrado!",
         user: req.body,
       });
     }
 
-    // const passed = await compare(password, user.password);
+    const passed = await compare(password, user[0].password);
 
-    // if (!passed) {
-    //   return res.render("session/login", {
-    //     error: "Senha incorreta!",
-    //     user: req.body,
-    //   });
-    // }
-
-    // console.log(user);
-
-    if (user[0].password != "123") {
+    if (!passed) {
       return res.render("session/login", {
         error: "Senha incorreta!",
         user: req.body,
@@ -55,6 +46,40 @@ async function checkFormLogin(req, res, next) {
 }
 async function checkFormReset(req, res, next) {
   try {
+    const { email, password, repeatPassword, token } = req.body;
+
+    const user = await User.find({ where: { email: email } });
+
+    let now = new Date();
+    now = now.setHours(now.getHours());
+
+    if (!user[0]) {
+      return res.render("session/reset-password", {
+        email: email,
+        token: token,
+        error: "Email não existe, por favor verifique o email digitado!",
+      });
+    }
+
+    if (now > user[0].reset_token_expires || token !== user[0].reset_token) {
+      return res.render("session/reset-password", {
+        email: email,
+        token: token,
+        error:
+          "Token inválido, por favor solicite novamente alteração de senha!",
+      });
+    }
+
+    if (password !== repeatPassword) {
+      return res.render("session/reset-password", {
+        email: email,
+        token: token,
+        error: "As senhas digitadas não são iguais!",
+      });
+    }
+
+    req.user = user[0];
+
     next();
   } catch (error) {
     console.error(error);
@@ -76,8 +101,21 @@ async function checkFormForgot(req, res, next) {
   }
 }
 
+async function checkingBeforeResettingPassword(req, res, next) {
+  try {
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.render("session/login", {
+      error: "Erro inesperado, tente novamente.",
+      user: req.body,
+    });
+  }
+}
+
 module.exports = {
   checkFormLogin,
   checkFormForgot,
   checkFormReset,
+  checkingBeforeResettingPassword,
 };
