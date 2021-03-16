b7a3e0b9d5e43908c47632dc89c63cb92d5adec8
Guilherme <guilherme_devz@hotmail.com>
2021-03-16 11:07:47 -0300

    ADD: creating the function 'put'

--- src/app/controllers/profileController.js
+++ src/app/controllers/profileController.js
@@ -3,13 +3,23 @@ const User = require("../models/user");
 
 module.exports = {
   async index(req, res) {
-    const user = await User.find({ where: { id: req.session.userID } });
-
     return res.render("admin/users/profile", {
       userIsAdmin: req.user.is_admin,
       user: req.user,
     });
   },
 
-  async put(req, res) {},
+  async put(req, res) {
+    const { name, email } = req.body;
+
+    const profile = { name: name, email: email };
+
+    await User.updating(req.user.id, profile);
+
+    return res.render("admin/users/profile", {
+      success: "Dados atualizados com sucesso!",
+      userIsAdmin: req.user.is_admin,
+      user: profile,
+    });
+  },
 };
